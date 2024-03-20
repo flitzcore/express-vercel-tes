96d026b2f628bdc6a3632fafd08d4b34ee5f74f1
lockrr <hemistergt@gmail.com>
2024-03-20 22:00:39 +0700

    delete morgan

--- api/app.js
+++ api/app.js
@@ -5,9 +5,10 @@ const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
 const passport = require('passport');
+
 const httpStatus = require('http-status');
 const config = require('./config/config');
-const morgan = require('./config/morgan');
+
 const { jwtStrategy } = require('./config/passport');
 const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
@@ -15,11 +16,6 @@ const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 
 const app = express();
-
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
 
 // set security HTTP headers
 app.use(helmet());
